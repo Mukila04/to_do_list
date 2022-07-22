9da6c4cba2d20fecd7aeefb765ae27e9bda27d54
Mukila04 <mukikty413121@gmail.com>
2022-07-22 21:41:53 +0530

    first projet

---
+++ script.js
@@ -0,0 +1,9 @@
+let myForm = document.getElementById("myForm"); 
+
+let myInput = document.getElementById("myInput");
+
+myForm.addEventListener("click",myFunction);
+
+function myFunction(){
+    console.log(myInput);
+}
